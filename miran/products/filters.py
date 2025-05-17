c205cd41718c496e5882cf2cb7f61eeb29546225
Omar Raafat <omarraafat14@gmail.com>
2025-05-17 14:34:37 +0300

    Add full-text multilingual search filter for products and update viewsets to utilize the new filter.

--- miran/products/filters.py
+++ miran/products/filters.py
@@ -1,8 +1,33 @@
+from django.contrib.postgres.search import SearchQuery, SearchRank, TrigramSimilarity
+from django.db.models import F, Q
+
+from .models import Product
 from django_filters import rest_framework as filters
 
-from . import models
 
-# class MODELNAMEFilter(filters.FilterSet):
-#    class Meta:
-#        model = MODELNAME
-#        fields = ["field",]
+class FullTextMultilingualSearchFilter(filters.CharFilter):
+    def filter(self, queryset, value):
+        if not value:
+            return queryset
+
+        search_query = SearchQuery(value, config="english") | SearchQuery(
+            value, config="arabic"
+        )
+
+        return (
+            queryset.annotate(
+                rank=SearchRank(F("search_vector"), search_query),
+                similarity=TrigramSimilarity("name", value)
+                + TrigramSimilarity("name_ar", value),
+            )
+            .filter(Q(search_vector=search_query) | Q(similarity__gt=0.3))
+            .order_by("-rank", "-similarity")
+        )
+
+
+class ProductFilter(filters.FilterSet):
+    search = FullTextMultilingualSearchFilter()
+
+    class Meta:
+        model = Product
+        fields = ["search", "brand", "category"]
--- miran/products/views.py
+++ miran/products/views.py
@@ -6,17 +6,19 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
     lookup_field = "slug"
+    search_fields = ["name__search"]
 
 
 class BrandViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Brand.objects.all()
     serializer_class = serializers.BrandSerializer
     lookup_field = "slug"
+    search_fields = ["name__search"]
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Product.objects.active()
     serializer_class = serializers.ProductSerializer
     lookup_field = "sku"
-    filterset_fields = ["category__slug", "brand__slug"]
+    filterset_class = filters.ProductFilter
     ordering_fields = ["price", "created_at"]
